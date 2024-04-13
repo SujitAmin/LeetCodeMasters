82f2f640a79db2df9cafc110e62f232c6f7acbee
Sujit Sundar Amin <143650989+sujitaminusc@users.noreply.github.com>
2024-04-13 12:40:44 -0700

    Remove Duplicates From Sorted Array

--- src/RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.js
+++ src/RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.js
@@ -1,15 +1,4 @@
-import os
-import sys
-
-def create_files(folder_name):
-    # Directory to hold the files
-    directory = os.path.join(os.getcwd(), folder_name)
-
-    # Create the directory if it doesn't exist
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    comment = """\
+/*
 26. Remove Duplicates from Sorted Array
 Solved
 Easy
@@ -62,26 +51,19 @@ Constraints:
     1 <= nums.length <= 3 * 104
     -100 <= nums[i] <= 100
     nums is sorted in non-decreasing order.
-  """
+  
+*/
 
-    # Define file names and their corresponding content templates
-    templates = {
-        ".java": "/*\n{0}\n*/\n\npublic class {1} {{\n    public static void main(String[] args) {{\n        // Code goes here\n    }}\n}}",
-        ".js": "/*\n{0}\n*/\n\nfunction main() {{\n    // Code goes here\n}}\nmain();",
-        ".py": "'''\n{0}\n'''\n\ndef main():\n    # Code goes here\n\nif __name__ == '__main__':\n    main()",
-        ".swift": "/*\n{0}\n*/\n\nimport Foundation\n\nfunc main() {{\n    // Code goes here\n}}\n\nmain()"
+var removeDuplicates = (nums) => {
+    if (nums.length === 1) {
+        return 1;
     }
-
-    # Create files with templates
-    for extension, content in templates.items():
-        file_path = os.path.join(directory, folder_name + extension)
-        formatted_content = content.format(comment, folder_name)
-        with open(file_path, 'w') as file:
-            file.write(formatted_content)
-        print(f"Created: {file_path}")
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python create_files.py [folder_name]")
-    else:
-        create_files(sys.argv[1])
+    let uniqueValueIndex = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[uniqueValueIndex] = nums[i];
+            uniqueValueIndex++;
+        }
+    }
+    return uniqueValueIndex;
+};
