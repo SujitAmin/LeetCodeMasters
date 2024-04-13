f880a3c20cf8db61bc4730035f9c6ff5fb84b3a6
Sujit Sundar Amin <143650989+sujitaminusc@users.noreply.github.com>
2024-04-12 20:27:21 -0700

    Fizz Buzz

--- src/create_files.py
+++ src/create_files.py
@@ -10,34 +10,38 @@ def create_files(folder_name):
         os.makedirs(directory)
 
     comment = """\
-217. Contains Duplicate
+412. Fizz Buzz
 Solved
 Easy
 Topics
 Companies
 
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Given an integer n, return a string array answer (1-indexed) where:
+
+    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    answer[i] == "Fizz" if i is divisible by 3.
+    answer[i] == "Buzz" if i is divisible by 5.
+    answer[i] == i (as a string) if none of the above conditions are true.
 
  
 
 Example 1:
 
-Input: nums = [1,2,3,1]
-Output: true
+Input: n = 3
+Output: ["1","2","Fizz"]
 
 Example 2:
 
-Input: nums = [1,2,3,4]
-Output: false
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
 
 Example 3:
 
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-Output: true
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
 
 Constraints:
-    1 <= nums.length <= 105
-    -109 <= nums[i] <= 109
+    1 <= n <= 104
   """
 
     # Define file names and their corresponding content templates
